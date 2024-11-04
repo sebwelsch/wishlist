973811445673bc36715dac3f3eb209a5690589b9
Sebastian <sebjak1@hotmail.com>
2024-11-04 20:12:36 +0100

    WIP login

--- src/main/java/keac6/wishlist/controller/WishListController.java
+++ src/main/java/keac6/wishlist/controller/WishListController.java
@@ -1,5 +1,28 @@
 package keac6.wishlist.controller;
 
+import keac6.wishlist.service.WishListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("")
 public class WishListController {
+    private final WishListService wishListService;
+
+    @Autowired
+    public WishListController(WishListService wishListService) {
+        this.wishListService = wishListService;
+    }
+
+    @GetMapping("/signup")
+    public String signUp() {
+        return "signUp";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
--- src/main/java/keac6/wishlist/repository/WishListRepository.java
+++ src/main/java/keac6/wishlist/repository/WishListRepository.java
@@ -1,4 +1,7 @@
 package keac6.wishlist.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class WishListRepository {
 }
